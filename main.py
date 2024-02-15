4568447f0a51722b8d8e85ca48345ad9a37c61c8
sajjin <sajjinnijjar@gmail.com>
2024-02-15 13:25:24 -0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import json
 import os
 import random
@@ -9,39 +10,39 @@ import cv2
 import pyautogui
 import numpy as np
 
-# Load the image of the object you want to detect
-template = cv2.imread('object.png', 0)
+os.chdir("/home/rewards/Documents/Reward_points-master/")
+template = cv2.imread('assets/object.png',0)
 w, h = template.shape[::-1]
 
 start_time = time.time()
-os.chdir("/home/rewards/Documents/Reward_points-master")
-data = json.load(open("dictionary.json"))
+data = json.load(open("assets/dictionary.json"))
 words = list(data.keys())
 
+def move_and_click(x, y):
+    pyautogui.moveTo(x, y, duration=random.uniform(0.1, 0.3))
+    pyautogui.click()
 
 def counter():
-    with open('counter_timer.txt') as ft:
+    with open('assets/counter_timer.txt') as ft:
         counter = int(ft.read())
-        with open('counter_timer.txt', 'w') as fh:
+        with open('assets/counter_timer.txt', 'w') as fh:
             counter += 1
             fh.write(str(counter))
         if counter == 49:
-            with open('dictionary.txt', 'w') as fs:
+            with open('assets/dictionary.txt', 'w') as fs:
                 for line in words:
                     fs.write(line)
                     fs.write("\n")
-            with open('counter_timer.txt', 'w') as fh:
+            with open('assets/counter_timer.txt', 'w') as fh:
                 fh.write("0")
 
-
 def firefox_browser():
-    with open('dictionary.txt') as f:
+    with open('assets/dictionary.txt') as f:
         contents = f.read()
         contents = contents.lower()
         contents = contents.split("\n")
         remove_list = []
         a = 0
-
         browser = webbrowser.Mozilla("/snap/bin/firefox")
         browser.open("https://bing.com")
         time.sleep(5)
@@ -59,9 +60,8 @@ def firefox_browser():
             time.sleep(random.randint(5, 10))
         remove_words(contents, remove_list)
 
-
 def edge_browser():
-    with open('dictionary.txt') as f:
+    with open('assets/dictionary.txt') as f:
         contents = f.read()
         contents = contents.lower()
         contents = contents.split("\n")
@@ -69,6 +69,8 @@ def edge_browser():
         b = 0
         browser = webbrowser.Chrome("/opt/microsoft/msedge/msedge")
         browser.open("https://bing.com")
+        template = cv2.imread('assets/object.png',0)
+        w, h = template.shape[::-1]
         while b != 60:
             word = random.choice(contents)
             if word not in remove_list:
@@ -76,31 +78,32 @@ def edge_browser():
             elif word in remove_list:
                 word = random.choice(contents)
             time.sleep(5)
-            if b == 30:
-                os.system("kill $(pidof msedge)")
-                time.sleep(5)
-            # Take a screenshot
-            screenshot = pyautogui.screenshot()
+            try:
+                screenshot = pyautogui.screenshot()
+            except:
+                screenshot = pyautogui.screenshot()
             screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
-            screenshot_gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
-            # Perform template matching
-            res = cv2.matchTemplate(screenshot_gray, template, cv2.TM_CCOEFF_NORMED)
+            screenshot_grey = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
+            res = cv2.matchTemplate(screenshot_grey, template, cv2.TM_CCOEFF_NORMED)
             threshold = 0.8
             loc = np.where(res >= threshold)
-            # Click and type on the detected object
             for pt in zip(*loc[::-1]):
-                pyautogui.click(pt[0] + w/2, pt[1] + h/2)
+                move_and_click(pt[0] - 20, pt[1])
                 pyautogui.write(word)
-                pyautogui.press('enter')
-            browser.open_new_tab(home_url) # replace home_url with the URL of the home page
+                time.sleep(1)
+                if b == 0:
+                    pyautogui.press('enter')
+                move_and_click(pt[0] - 100, pt[1] + 100)
+            time.sleep(random.randint(1, 5))
+            template = cv2.imread('assets/mic.png',0)
+            w, h = template.shape[::-1]
             b += 1
             time.sleep(random.randint(5, 10))
         remove_words(contents, remove_list)
 
-
 def remove_words(contents, remove_list):
     keep_list = list(set(contents) - set(remove_list))
-    with open('dictionary.txt', 'w') as fa:
+    with open('assets/dictionary.txt', 'w') as fa:
         for line in keep_list:
             fa.write(line)
             fa.write("\n")
@@ -110,7 +113,6 @@ def GUI():
         [sg.Button("Run PC only")],
         [sg.Button("Run Mobile only")]
     ]
-
     window = sg.Window("Rewards", layout)
     while True:
         event, values = window.read(timeout = 1000 * 15)
@@ -120,11 +122,9 @@ def GUI():
             edge_browser()
             time.sleep(10)
             os.system("kill $(pidof msedge)")
-
             firefox_browser()
             time.sleep(10)
             os.system("kill $(pidof firefox)")
-            
             os.system("shutdown")
         elif event == sg.WIN_CLOSED:
             window.close()
@@ -143,7 +143,6 @@ def GUI():
             os.system("kill $(pidof firefox)")
             os.system("shutdown")
             break
-        
 
 def main():
     counter()
